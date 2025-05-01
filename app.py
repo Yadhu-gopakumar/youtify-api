824e79fc7e9c30790edf73bb405656f214da3fc1
Yadhu gopakumar <87005083+Yadhu-gopakumar@users.noreply.github.com>
2025-05-01 14:26:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 from ytmusicapi import YTMusic
+import subprocess
 
 app = Flask(__name__)
 CORS(app)  
@@ -9,6 +10,24 @@ CORS(app)
 @app.route('/')
 def home():
     return jsonify({"message": "Youtify music app api-yadhu"})
+
+
+def get_audio_url(video_id):
+    try:
+        cmd = [
+            'yt-dlp',
+            '-f', 'bestaudio',
+            '--get-url',
+            f'https://www.youtube.com/watch?v={video_id}'
+        ]
+        result = subprocess.check_output(cmd, timeout=10).decode('utf-8').strip()
+        return result
+    except Exception as e:
+        print(f"Error fetching audio URL for {video_id}: {e}")
+        return None
+
+
+
 # Fetch popular or trending songs
 @app.route('/trending', methods=['GET'])
 def trending():
@@ -18,12 +37,13 @@ def trending():
     try:
         # Search for popular songs or playlists (Example search for 'trending')
         search_results = ytmusic.search('trending', limit=10)
-
+        audio_url = get_audio_url(video_id)
         # Format the response to return only the song title and videoId
         trending_results = [{
             'title': song['title'],
             'videoId': song['videoId'],
-            'artists': [artist['name'] for artist in song['artists']]
+            'artists': [artist['name'] for artist in song['artists']],
+            'audioUrl': audio_url
         } for song in search_results if 'videoId' in song]
 
         return jsonify(trending_results)
@@ -45,12 +65,13 @@ def search():
     try:
         # Perform the search based on the query
         search_results = ytmusic.search(query, limit=limit)
-
+        audio_url = get_audio_url(video_id)
         # Format the response to return only relevant details like title, videoId, and artists
         search_results_formatted = [{
             'title': song['title'],
             'videoId': song['videoId'],
-            'artists': [artist['name'] for artist in song['artists']]
+            'artists': [artist['name'] for artist in song['artists']],
+            'audioUrl': audio_url
         } for song in search_results if 'videoId' in song]
 
         return jsonify(search_results_formatted)
